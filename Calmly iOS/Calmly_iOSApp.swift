1068d80cb0c09cbfb61cabf8502dad9d014eca85
Roisin Yates <roisinyates@outlook.com>
2023-05-11 16:17:25 +1200

    Initial Commit

---
+++ Calmly iOS/Calmly_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calmly_iOSApp.swift
+//  Calmly iOS
+//
+//  Created by Roisin Yates on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Calmly_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calmly iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Calmly iOS
+//
+//  Created by Roisin Yates on 11/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
