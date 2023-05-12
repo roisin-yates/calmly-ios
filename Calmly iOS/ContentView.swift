81abdb4ed5511b7df94a01b5f1a7109ec6028594
Roisin Yates <roisinyates@outlook.com>
2023-05-12 12:53:45 +1200

    Init

--- Calmly iOS/ContentView.swift
+++ Calmly iOS/ContentView.swift
@@ -7,20 +7,36 @@
 
 import SwiftUI
 
+let mainColor = Color(red: 1, green: 142/255, blue: 147/255)
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            mainColor
+            VStack {
+                Text("Welcome to Calmly")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                    .padding()
+                Button(action: {
+                    print("You've logged in")
+                }, label: {
+                    Text("Login")
+                        .padding()
+                        .background(.yellow)
+                })
+            }
+            .foregroundColor(.white)
         }
-        .padding()
+        
+        
+            
+            
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
